ffcba8735a23c7d49857a4cc796157a68eb7deb4
Victor Yunenko <victor@what.digital>
2019-10-09 10:48:59 +0300

    add comas

--- frontend/global/djangocms-reload.ts
+++ frontend/global/djangocms-reload.ts
@@ -1,12 +1,12 @@
-declare var CMS: any
+declare var CMS: any;
 
 
 if (window.hasOwnProperty('CMS')) {
     CMS.$(window).on('cms-content-refresh', () => {
         $('script[data-cms-reload]').each(() => {
-            let src = $(this).attr('src')
-            $(this).remove()
-            $('<script>').attr('src', src).appendTo('head')
+            let src = $(this).attr('src');
+            $(this).remove();
+            $('<script>').attr('src', src).appendTo('head');
         })
     })
 }
