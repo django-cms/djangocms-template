4d0c269f72c151b73e57cfe723223d9e31984fbb
Victor Yunenko <victor@what.digital>
2019-10-13 15:51:22 +0300

    add a comment

--- frontend/global/ts/djangocms-reload.ts
+++ frontend/global/ts/djangocms-reload.ts
@@ -1,6 +1,9 @@
 declare var CMS: any;
 
 
+/**
+ * It's a cms reloading fix, probably was written by Mario. What it fixes and whether it's actual right now is unclear.
+ */
 if (window.hasOwnProperty('CMS')) {
     CMS.$(window).on('cms-content-refresh', () => {
         $('script[data-cms-reload]').each(() => {
