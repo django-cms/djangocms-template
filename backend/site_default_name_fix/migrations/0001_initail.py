dfb7bc3dbfaf043405d3641cb5c61179ac257f65
Victor Yunenko <victor@what.digital>
2019-10-19 15:33:31 +0300

    add divio support

--- backend/site_default_name_fix/migrations/0001_initail.py
+++ backend/site_default_name_fix/migrations/0001_initail.py
@@ -9,20 +9,24 @@ def change_default_site_name(apps: Apps, _):
     Site = apps.get_model('sites', 'Site')
     site = Site.objects.first()
     
-    if settings.BASE_URL is None:
+    sites_domains = getattr(settings, 'ALDRYN_SITES_DOMAINS', None)
+    if settings.BASE_URL is None or sites_domains:
         return
     
     if site is None:
         site = Site()
 
-    is_protocol_missing = (
-        'http://' not in settings.BASE_URL and
-        'https://' not in settings.BASE_URL
-    )
-    if is_protocol_missing:
-        domain = "{0.netloc}".format(urlsplit(f'https://{settings.BASE_URL}'))
+    if settings.BASE_URL:
+        is_protocol_missing = (
+            'http://' not in settings.BASE_URL and
+            'https://' not in settings.BASE_URL
+        )
+        if is_protocol_missing:
+            domain = "{0.netloc}".format(urlsplit(f'https://{settings.BASE_URL}'))
+        else:
+            domain = "{0.netloc}".format(urlsplit(settings.BASE_URL))
     else:
-        domain = "{0.netloc}".format(urlsplit(settings.BASE_URL))
+        domain = sites_domains[0]['domain']
 
     site.domain = domain
     site.name = domain
