cd85b8f2934b913ea58a3df39a8e770cc297690b
Victor Yunenko <victor@what.digital>
2019-12-24 18:25:47 +0200

    fix auth admin

--- backend/auth/admin.py
+++ backend/auth/admin.py
@@ -1,25 +1,7 @@
+from cuser.admin import UserAdmin
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 
 from backend.auth.models import User
 
 
-@admin.register(User)
-class UserAdmin(ModelAdmin):
-    list_display = [
-        'email',
-        'first_name',
-        'last_name',
-        'is_active',
-        'is_staff',
-        'is_superuser',
-        'date_joined',
-        'last_login',
-    ]
-    list_filter = [
-        'is_active',
-        'date_joined',
-        'last_login',
-        'is_staff',
-        'is_superuser',
-    ]
+admin.site.register(User, UserAdmin)
