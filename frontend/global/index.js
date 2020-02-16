febe05aa29d653ece7f12bc90f4d8a93fd206dd1
Mario Colombo <mario@what.digital>
2020-02-16 19:01:53 +0100

    add bs4 customisation possibility and fix 24 grid

--- frontend/global/index.js
+++ frontend/global/index.js
@@ -1,2 +1,7 @@
+// Styles
 require('./scss/main.scss');
+require('./scss/font-awesome.sass');
+
+// TS
 require('./ts/on-page-edit-reload');
+
--- frontend/vendor/index.js
+++ frontend/vendor/index.js
@@ -1,1 +1,38 @@
-require('bootstrap/scss/bootstrap.scss');
+// this will let us have a global jQuery object
+window.$ = window.jQuery = require("jquery");
+
+
+// bootstrap4
+require('bootstrap');
+require('../global/scss/bootstrap4-custom.sass');
+
+// Icons
+require('@fortawesome/fontawesome-free/scss/fontawesome.scss');
+require('@fortawesome/fontawesome-free/scss/brands.scss');
+require('@fortawesome/fontawesome-free/scss/solid.scss');
+require('@fortawesome/fontawesome-free/scss/regular.scss');
+
+
+// DEMO tools that frontend engineers like
+// if you want to install one of them cd to the frontend directory and: npm i <name> --save
+
+// window.enquire = require('enquire.js');
+// window.FastClick = require('fastclick');
+// require('jquery-debouncedresize');
+// require('imagesloaded');
+// require('jquery-match-height');
+// window.Cookies = require('js-cookie');
+// window.svg4everybody = require('svg4everybody');
+// window.objectFitImages = require('object-fit-images');
+// window.autosize = require('autosize');
+
+// require('slick-carousel/slick/slick.scss');
+// require('slick-carousel/slick/slick-theme.scss');
+// require('slick-carousel');
+
+
+// Modernizr global :)
+// window.Modernizr = require('modernizr');
+
+// window.moment = require('moment');
+// require('moment-timezone');
