661706311978337cfb5f8884b5916123ee868e17
Victor Yunenko <victor@what.digital>
2020-03-05 13:48:06 +0200

    enable django robots

--- urls.py
+++ urls.py
@@ -2,11 +2,10 @@ import aldryn_addons.urls
 from aldryn_django.utils import i18n_patterns
 from django.conf import settings
 from django.urls import include
-from django.urls import path
 from djangocms_helpers.sentry_500_error_handler.views import collect_500_error_user_feedback_view
 
 urlpatterns = [
-    path('marketplace/', include('backend.marketplace.urls')),
+    url(r'^robots\.txt', include('robots.urls')),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns(),  # MUST be the last entry!
