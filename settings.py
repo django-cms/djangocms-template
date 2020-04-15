1c6a88d9207d1806444c3ee203dae10793a8660d
Victor Yunenko <victor@what.digital>
2020-04-15 15:17:13 +0300

    enable widget_tweaks

--- settings.py
+++ settings.py
@@ -82,6 +82,7 @@ INSTALLED_APPS.extend([
     'adminsortable2',
     'admin_reorder',
     'django_extensions',
+    'widget_tweaks',
     'django_countries',
     'logentry_admin',
     'hijack_admin',
