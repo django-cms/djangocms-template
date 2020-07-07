4aec8a769ac20b8cb90db1f9183d841d380eea49
Victor Yunenko <victor@what.digital>
2020-07-07 17:48:47 +0300

    cleanup linktypes patches

--- backend/plugins/link/link_types.py
+++ backend/plugins/link/link_types.py
@@ -2,15 +2,7 @@ from djangocms_blog.models import Post
 from linkit.types.model import ModelLinkType
 
 
-class PatchedModelLinkType(ModelLinkType):
-    def real_value(self):
-        if isinstance(self.link.data('value').get('model'), int):
-            return self.model.objects.filter(pk=self.link.data('value').get('model')).first()
-        else:
-            return self.model.objects.filter(pk=self.link.data('value').get('model').pk).first()
-
-
-class DjangocmsBlogPostLinkType(PatchedModelLinkType):
+class DjangocmsBlogPostLinkType(ModelLinkType):
     identifier = 'djangocms_blog'
     type_label = 'Story'
     model = Post
