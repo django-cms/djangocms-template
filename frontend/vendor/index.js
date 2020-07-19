62432092dbb3c4e2783838586eb0a6e6ca09ff64
Victor Yunenko <victor@what.digital>
2020-07-19 20:25:44 +0300

    frontend: clarify the dir structure

--- frontend/vendor/index.js
+++ frontend/vendor/index.js
@@ -1,15 +1,14 @@
 import * as Sentry from '@sentry/browser';
 
 
-// this will let us have a global jQuery object
-window.$ = window.jQuery = require("jquery");
+window.$ = window.jQuery = require('jquery');
 
 
 require('bootstrap');
-require('./scss/main.scss');
+require('./main.scss');
 
 
-// Icons
+// djangocms-bootstrap4 icons
 require('@fortawesome/fontawesome-free/scss/fontawesome.scss');
 require('@fortawesome/fontawesome-free/scss/brands.scss');
 require('@fortawesome/fontawesome-free/scss/solid.scss');
@@ -20,28 +19,3 @@ Sentry.init({
     dsn: '',
     environment: DJANGO.env,
 });
-
-
-// DEMO tools that frontend engineers like
-// if you want to install one of them cd to the frontend directory and: npm i <name> --save
-
-// window.enquire = require('enquire.js');
-// window.FastClick = require('fastclick');
-// require('jquery-debouncedresize');
-// require('imagesloaded');
-// require('jquery-match-height');
-// window.Cookies = require('js-cookie');
-// window.svg4everybody = require('svg4everybody');
-// window.objectFitImages = require('object-fit-images');
-// window.autosize = require('autosize');
-
-// require('slick-carousel/slick/slick.scss');
-// require('slick-carousel/slick/slick-theme.scss');
-// require('slick-carousel');
-
-
-// Modernizr global :)
-// window.Modernizr = require('modernizr');
-
-// window.moment = require('moment');
-// require('moment-timezone');
