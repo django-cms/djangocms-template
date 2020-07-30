b09e69f4cbaf1558cde9934d403cd47b0ba1ed0f
Victor Yunenko <victor@what.digital>
2020-07-30 14:17:46 +0300

    improve dir structure and comments

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,7 +15,7 @@ const config = {
         global: './frontend/global/index.js',
         vendor: './frontend/vendor/index.js',
 
-        plugin_demo: './frontend/plugins/demo-plugin/index.js',
+        component_demo: './frontend/components/demo-plugin/index.js',
     },
     output: {
         filename: '[name].js',
