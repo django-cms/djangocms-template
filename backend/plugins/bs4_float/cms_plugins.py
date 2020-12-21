1d333dba0abdb0461b5625728e655f9aa6db1f8d
Victor Yunenko <victor@what.digital>
2020-12-21 19:59:04 +0200

    add isort and pyproject.tml

--- backend/plugins/bs4_float/cms_plugins.py
+++ backend/plugins/bs4_float/cms_plugins.py
@@ -1,9 +1,9 @@
-from django.utils.translation import ugettext_lazy as _
-
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
 
 from backend.plugins.module_name import MODULE_NAME
+
 from . import models
 
 
--- backend/plugins/bs4_spacer/cms_plugins.py
+++ backend/plugins/bs4_spacer/cms_plugins.py
@@ -3,6 +3,7 @@ from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 
 from backend.plugins.module_name import MODULE_NAME
+
 from .models import VerticalSpacerPlugin
 
 
